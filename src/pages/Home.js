b0ca4c38bc87f73a75d97f9d81598e6dcade76ed
samseburn <90seok_0@naver.com>
2022-12-15 19:34:19 +0900

    ✨ Feat: react-router-dom으로 페이지 이동 및 연결

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import { db } from '../utils/firebase';
+import BookList from '../components/BookList';
+import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { BookDataContext } from '../App';
 
 const Home = () => {
-  return (
-    <div>
-      여기는 홈 페이지 입니다. 
-    </div>
-  );
+	const navigate = useNavigate();
+	const bookData = useContext(BookDataContext);
+
+	const goNew = () => {
+		navigate(`/new`);
+	};
+
+	return (
+		<div className="Home">
+			<button onClick={goNew}>추가하기</button>
+			<BookList bookData={bookData} />
+		</div>
+	);
 };
 
 export default Home;
--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,5 +1,4 @@
 import { Outlet } from 'react-router-dom';
-
 import Header from '../common/Header';
 
 const Root = () => {
