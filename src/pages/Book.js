e9f3db3c84629684a785cd566e4f641814dac92b
Ko Seokyoung <68809452+samseburn@users.noreply.github.com>
2022-12-23 19:21:13 +0900

    ✨ Feat: 독서 정보 삭제 기능 추가

--- src/pages/Book.js
+++ src/pages/Book.js
@@ -1,14 +1,13 @@
 import React, { useState, useContext, useEffect, useRef } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { BookDataContext } from '../App';
+import { BookDataContext, BookDispatchContext } from '../App';
 
 const Book = () => {
 	const navigate = useNavigate();
 	const { bookId } = useParams();
-	const idRef = useRef();
-	console.log(bookId);
 
 	const bookData = useContext(BookDataContext);
+	const { onRemove } = useContext(BookDispatchContext);
 	const [originData, setOriginData] = useState();
 
 	const goEdit = () => {
@@ -18,12 +17,19 @@ const Book = () => {
 	useEffect(() => {
 		if (bookData.length >= 1) {
 			// 데이터 있을 경우
-			const targetBook = bookData.find(book => book.docID === bookId);
-			idRef.current = targetBook;
+			const targetBook = bookData.find((book) => book.docID === bookId);
 
 			setOriginData(targetBook);
 		}
 	}, [bookId, bookData]);
+
+	const handleRemove = () => {
+		if (window.confirm(`해당 독서 정보를 삭제하시겠습니까? `)) {
+			alert(`삭제를 완료했습니다.`);
+			onRemove(bookId);
+			navigate('/', { replace: true });
+		}
+	};
 
 	if (!originData) {
 		return <div>Page Not Found</div>;
@@ -33,13 +39,7 @@ const Book = () => {
 				여기는 책의 상세 정보를 볼 수 있는 페이지 입니다.
 				<span>ID: {bookId}</span>
 				<button onClick={goEdit}>수정하기</button>
-				<button
-					onClick={() => {
-						navigate('/', { replace: true });
-					}}
-				>
-					삭제하기
-				</button>
+				<button onClick={handleRemove}>삭제하기</button>
 				<div
 					style={{
 						width: 500,
