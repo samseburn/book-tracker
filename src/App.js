cc452437ac2b5e30fea381662b31c03b2b1129a3
Ko Seokyoung <68809452+samseburn@users.noreply.github.com>
2022-12-25 17:55:41 +0900

    🚧 Feat: 독서 정보 CRUD 기능

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { collection, getDocs } from 'firebase/firestore';
+import {
+	collection,
+	getDocs,
+	addDoc,
+	doc,
+	deleteDoc,
+	updateDoc,
+} from 'firebase/firestore';
 import { db } from './utils/firebase';
 import './App.css';
 
@@ -11,6 +18,7 @@ import Edit from './pages/Edit';
 import Book from './pages/Book';
 
 export const BookDataContext = React.createContext();
+export const BookDispatchContext = React.createContext();
 
 const router = createBrowserRouter([
 	{
@@ -21,16 +29,16 @@ const router = createBrowserRouter([
 			{ index: true, element: <Home /> },
 			{ path: '/new', element: <New /> },
 			{ path: '/edit/:bookId', element: <Edit /> },
-			{ path: '/book/', element: <Book /> },
+			{ path: '/book/:bookId', element: <Book /> },
 		],
 	},
 ]);
 
 function App() {
+	// 전체 데이터 관리
 	useEffect(() => {
 		console.log(db);
 	}, []);
-
 	const bookCollection = collection(db, 'books');
 	const [bookData, setBookData] = useState([
 		{
@@ -44,17 +52,71 @@ function App() {
 	// db 데이터 받아오기
 	const getBooks = async () => {
 		const data = await getDocs(bookCollection);
-		setBookData(data.docs.map(doc => ({ ...doc.data(), docID: doc.id })));
+		setBookData(data.docs.map((doc) => ({ ...doc.data(), docID: doc.id })));
 		// console.log(bookData);
 	};
 
 	useEffect(() => {
 		getBooks();
 		console.log(bookData);
+	}, [bookData.length]);
+
+	// CREATE
+	const onCreate = async (book) => {
+		const newData = {
+			title: book.title,
+			author: book.author,
+			publisher: book.publisher,
+			year: book.year,
+			date: book.date,
+		};
+
+		await addDoc(bookCollection, {
+			title: newData.title,
+			author: newData.author,
+			publisher: newData.publisher,
+			year: newData.year,
+			date: newData.date,
+		});
+
+		setBookData([...bookData, newData]);
+	};
+
+	// REMOVE
+	const onRemove = async (targetId) => {
+		await deleteDoc(doc(bookCollection, targetId));
+		getBooks();
+	};
+
+	// EDIT
+	const onEdit = async (targetId, book) => {
+		const updateData = {
+			title: book.title,
+			author: book.author,
+			publisher: book.publisher,
+			year: book.year,
+			date: book.date,
+		};
+		console.log(targetId);
+		console.log(book);
+		await updateDoc(doc(bookCollection, targetId), updateData);
+		getBooks();
+	};
+
+	// context 최적화
+	const memoizedDispatches = useMemo(() => {
+		return {
+			onCreate,
+			onRemove,
+			onEdit,
+		};
 	}, []);
+
 	return (
 		<BookDataContext.Provider value={bookData}>
-			<RouterProvider router={router} />
+			<BookDispatchContext.Provider value={memoizedDispatches}>
+				<RouterProvider router={router} />
+			</BookDispatchContext.Provider>
 		</BookDataContext.Provider>
 	);
 }
