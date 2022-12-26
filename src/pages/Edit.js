28dc525c481a6fc619a39c66867132f2dcd11ba0
Ko Seokyoung <68809452+samseburn@users.noreply.github.com>
2022-12-26 09:37:10 +0900

    🚧 Feat: 상세 독서 정보가 존재하는지 판별 및 판별 결과 props로 전달

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { BookDataContext } from '../App';
+import { BookEditor } from '../components/BookEditor';
 
 const Edit = () => {
-  return (
-    <div>
-      여기는 기존 책 정보를 수정하는 페이지 입니다. 
-    </div>
-  );
+	const [originData, setOriginData] = useState();
+	const { bookId } = useParams();
+
+	const bookData = useContext(BookDataContext);
+
+	// id 조회
+	useEffect(() => {
+		if (bookData.length >= 1) {
+			// 데이터 있을 경우
+			const targetBook = bookData.find((book) => book.docID === bookId);
+
+			if (targetBook) {
+				setOriginData(targetBook);
+			}
+			// else {
+			// 	alert('찾으시는 책의 정보가 없습니다.');
+			// 	navigate('/', { replace: true });
+			// }
+		}
+	}, [bookId, bookData]);
+
+	return (
+		<div>
+			{originData && <BookEditor originData={originData} isEdit={true} />}
+		</div>
+	);
 };
 
 export default Edit;
