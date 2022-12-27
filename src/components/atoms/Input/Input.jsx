7f6ece299af9579d25ee6fc8877c248fb79c419b
Ko Seokyoung <68809452+samseburn@users.noreply.github.com>
2022-12-27 21:22:16 +0900

    💄 input 스타일 변경 및 focus 상태 스타일 추가

--- src/components/atoms/Input/Input.jsx
+++ src/components/atoms/Input/Input.jsx
@@ -1,13 +1,18 @@
 import styled from 'styled-components';
-import { COLORS } from '../../../styles';
+import { COLORS } from 'styles';
 
 const Input = styled.input`
 	display: block;
-	padding: 4px 16px;
+	padding: 0px 16px;
 	width: 100%;
-	background-color: ${COLORS.background};
-	border: 8px;
-	border: 1px solid ${COLORS.border};
+	height: 32px;
+	color: ${COLORS.primary};
+	background-color: ${COLORS.white};
+	border: 1px solid gray;
+
+	&:focus {
+		border: 1px solid rgba(0, 102, 255, 1);
+	}
 `;
 
 export default Input;
